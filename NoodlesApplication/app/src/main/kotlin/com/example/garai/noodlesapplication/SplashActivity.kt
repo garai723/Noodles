dcc998a40540d45c46ae7a7703302ef7f295eac2
新井 元気 <garai@crooz.local>
2016-11-18 13:40:52 +0900

    スプラッシュ作成

--- NoodlesApplication/app/src/main/kotlin/com/example/garai/noodlesapplication/SplashActivity.kt
+++ NoodlesApplication/app/src/main/kotlin/com/example/garai/noodlesapplication/SplashActivity.kt
@@ -1,12 +1,33 @@
 package com.example.garai.noodlesapplication
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.ActivityCompat.startActivity
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Window
+
 
 class SplashActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed(delayFunc, 2000)
+
+
     }
+
+    val delayFunc = Runnable {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
+
+
